31f611b9887ad89362a4902cb86eb05b0a4d59cd
sw-shar <reklama.zap@ya.ru>
2022-02-11 18:01:53 +0300

    run.sh without cp --parents

--- task2/run.sh
+++ task2/run.sh
@@ -2,29 +2,36 @@
 
 set -e
 
-[ $# = 4 ]
+[ $# = 4 ] # проверка на правильность ввода
 src=$1
 ext=$2
 dst=$3
 tarball=$4
 
 mkdir "$dst"
-dst_real=$(realpath "$dst")
+dst_real=$(realpath "$dst") # realpath - вернуть канонизированный абсолютный путь
 
 (
-	cd "$src"
-	find -name "*.$ext" -print0 |
-		xargs -0 cp -a --parents -t "$dst_real"
+  cd "$src"
+  find -name "*.$ext" |
+    while read -r path; do  # e.g.: a/b/c.cpp
+      dir=$(dirname "$path")  # a/b
+      base=$(basename "$path")  # c.cpp
+    
+      dir_dst=$dst_real/$dir  # .../dst/a/b
+      mkdir -p "$dir_dst"
+    
+      path_dst=$dir_dst/$base  # .../dst/a/b/c.cpp
+      cp -a "$path" "$path_dst"
+    done
 )
 
 tarball_real=$(realpath "$tarball")
-rm -f "$tarball_real"
-
-dst_base=$(basename "$dst")
+dst_base=$(basename "$dst") #basename парсить компоненты имени пути
 
 (
-	cd "$dst"/..
-	tar -cz -f "$tarball_real" "$dst_base"
+  cd "$dst"/..
+  tar -cz -f "$tarball_real" "$dst_base"
 )
 
 echo done
