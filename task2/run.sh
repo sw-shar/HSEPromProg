f92728a7a889012317ee48297c93f8c3696b4a7f
sw-shar <reklama.zap@ya.ru>
2022-02-11 16:26:57 +0300

    update run.sh for task2

--- task2/run.sh
+++ task2/run.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-set -e  # exit on error          
-set -x  # trace
+set -e
 
-[ $# = 4 ]  # test: num. of args = 4
+[ $# = 4 ]
 src=$1
 ext=$2
 dst=$3
 tarball=$4
-# ./run.sh src cpp dst dst.tar.gz
 
 mkdir "$dst"
 dst_real=$(realpath "$dst")
 
-(  # subshell
-	# find ~/repo -name '*.cpp'
+(
 	cd "$src"
 	find -name "*.$ext" -print0 |
 		xargs -0 cp -a --parents -t "$dst_real"
@@ -29,3 +26,5 @@ dst_base=$(basename "$dst")
 	cd "$dst"/..
 	tar -cz -f "$tarball_real" "$dst_base"
 )
+
+echo done
